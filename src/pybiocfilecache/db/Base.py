afb44fdfd1154fea6703f3ffe81dcc79347503ce
Kancherla <jayaram.kancherla@gmail.com>
2021-10-21 11:41:08 -0700

    fix: ignore sqlite thread check

--- src/pybiocfilecache/db/Base.py
+++ src/pybiocfilecache/db/Base.py
@@ -23,7 +23,7 @@ def create_schema(cache_dir:str):
     if not cache_dir:
         raise Exception(f"cache_dir cannot be empty")
 
-    engine = create_engine(f'sqlite:///{cache_dir}')
+    engine = create_engine(f'sqlite:///{cache_dir}', connect_args={"check_same_thread": False})
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     Base.metadata.create_all(engine)
     return (engine, SessionLocal)
